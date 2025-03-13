2e0f3f296857451463c6b7826fc7efa7d7644770
ShivamMathtech <drshivam49@gmail.com>
2025-03-13 20:24:50 +0530

    fontend is added

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const incomeRouter = require("./src/router/IncomeRoter");
 const reportRouter = require("./src/router/reportsRouter");
 const budgetRouter = require("./src/router/bugetRouter");
 app.use(cors());
-app.use(express.static(path.join(__dirname, "../src/frontend")));
+app.use(express.static(path.join(__dirname, "/src/public")));
 app.use(express.json());
 app.use("/api", authRouter);
 app.use("/api", expenseRouter);
@@ -19,8 +19,14 @@ app.use("/api", incomeRouter);
 app.use("/api", reportRouter);
 app.use("/api", budgetRouter);
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "src/frontend/index.html"));
+  res.sendFile(path.join(__dirname, "src/frontend/", req.path));
 });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "src/frontend/", "login.html"));
+});
+// app.get("/home", (req, res) => {
+//   res.sendFile(path.join(__dirname, "src/frontend/home.html"));
+// });
 const port = process.env.PORT_NO || 3000;
 app.listen(port, () => {
   console.log(`Server is running on Port no ${port}`);
